838e665a5963bcc66b96438c1d5ba854e053e881
amyngb <amyngbedinghaus@gmail.com>
2019-01-25 20:55:47 -0500

    calculator can only send one number :-(

--- amy-bedinghaus/calculator/app/controllers/calculate_controller.rb
+++ amy-bedinghaus/calculator/app/controllers/calculate_controller.rb
@@ -4,6 +4,20 @@ class CalculateController < ApplicationController
 
     def calculate
         @num1 = params[:n1].to_i
+        @num2 = params[:n2].to_i
+        @operation = params[:op]
+
+        @result = ''
+        
+        case @operation
+        when '+'
+            @result = @num1 + @num2
+        when '-'
+            @result = @num1 - @num2
+        else
+            @result = ''
+        end           
+
         render :index
     end
 end
